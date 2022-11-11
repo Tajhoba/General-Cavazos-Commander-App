f95bf7960d5c1ccd77e37e26da6e0ab4392b26f7
Tajhoba Williams <89955965+Tajhoba@users.noreply.github.com>
2022-11-10 18:25:49 -0800

    public class

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,7 @@
-package com.example;
+package com.student;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
+import org.json.simple.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
